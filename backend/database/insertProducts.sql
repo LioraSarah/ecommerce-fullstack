2cbea321626fb75d2c8eece104d906e83c1609c6
LioraSarah <liorasarahferrero@gmail.com>
2023-02-08 18:27:04 +0200

    added data

---
+++ backend/database/insertProducts.sql
@@ -0,0 +1,12 @@
+INSERT INTO products (id, product_name, price, category, material, quantity, image_url)
+    VALUES (DEFAULT,'Butterfly Top',30,'tops','100% Cotton',10,'butterfly'),
+    (DEFAULT,'Among The Crowd Top',35,'tops','Polyester Blend',10,'among_the_crowd'),
+    (DEFAULT,'Tapissa One-Shoulder Top',50,'tops','100% Polyester',10,'tapissa'),
+    (DEFAULT,'First In Line Shrug',25,'tops','Polyester Blend',10,'first_in_line'),
+    (DEFAULT,'High-Waisted Shorts',30,'bottoms','56% Cotton, 42% Polyester, 2% Viscose',10,'high_waist_shorts'),
+    (DEFAULT,'Butterfly Pants',20,'bottoms','100% Cotton',10,'forever_young_butterfly'),
+    (DEFAULT,'Tangerine Bucket Hat',13,'accessories','100% Cotton',10,'Tangerine_Laid_Back_Bucket_Hat'),
+    (DEFAULT,'Violet Socks',10,'accessories','38% Acrylic, 37% Polyester, 17% Nylon, 8% Spandex',10,'violet_socks'),
+    (DEFAULT,'Oatmeal Gloves',10,'accessories','100% Acrylic',10,'oatmeal_gloves'),
+    (DEFAULT,'Dog Jumper',30,'pets','100% Polyester',10,'dog_jumper'),
+    (DEFAULT,'Cat Sweater',30,'pets','100% Cotton',10,'Cat_Sweater');
